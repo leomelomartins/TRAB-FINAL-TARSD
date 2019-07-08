83f873dd2db8a882d0a17a9a9eadc43020d7a00a
leomelomartins <leonardo.melo.mar@gmail.com>
2019-07-08 15:30:12 -0300

    CHANGES

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y python3
+sudo DEBIAN_FRONTEND=noninteractive apt install -y python3-pip
+sudo pip3 install -y flask
+sudo apt -y install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+
+git clone https://github.com/leomelomartins/TRAB-FINAL-TARSD
