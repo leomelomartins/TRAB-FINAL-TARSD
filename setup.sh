c77a4afe96fc151780a70738f13f804dcafb5769
bruno <brunobaradel@gmail.com>
2019-07-08 18:04:55 -0300

    miau

--- setup.sh
+++ setup.sh
@@ -6,9 +6,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt install -y python3
-sudo DEBIAN_FRONTEND=noninteractive apt install -y python3-pip
-sudo pip3 install -y flask
 sudo apt -y install docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
