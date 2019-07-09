6b61fb7866e22a54fb50742f3114800686c4e734
bruno <brunobaradel@gmail.com>
2019-07-09 15:08:38 -0300

    começando app2

--- master-config.sh
+++ master-config.sh
@@ -29,6 +29,7 @@ sudo docker network create -d overlay --subnet 10.0.10.0/24 ClusterNet
 #PROMETHEUS
 # sudo docker build -t my-prometheus .
 # sudo docker run -p 9090:9090 --restart=always --detach=true --name=prometheus my-prometheus
+
 # sudo docker run -d --restart=always --net="host" --pid="host" --publish=9100:9100 --detach=true --name=node-exporter -v "/:/host:ro,rslave" quay.io/prometheus/node-exporter --path.rootfs /host
 # sudo docker run --restart=always --volume=/:/rootfs:ro --volume=/var/run:/var/run:ro --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --volume=/dev/disk/:/dev/disk:ro --publish=8080:8080 --detach=true --name=cadvisor google/cadvisor:latest
 
