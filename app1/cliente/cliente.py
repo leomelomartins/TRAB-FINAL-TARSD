af6c9de1f05e9974e3bed3de9fe444bfd4db61c2
bruno <brunobaradel@gmail.com>
2019-07-08 21:17:57 -0300

    basjaks

--- app1/cliente/cliente.py
+++ app1/cliente/cliente.py
@@ -1,13 +1,15 @@
 import socket
 HOST = '192.168.50.2'
-PORT = 8220
+PORT =  8220
 dest = (HOST, PORT)
 
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp.connect(dest)
 
-msg = input()
-while msg != '\x18':
-    tcp.send (msg)
-    msg = raw_input()
+fname = 'arquivos/teste' + input() + '.txt'
+arq = open(fname, 'rb')
+
+for i in arq.readlines():
+  client_socket.send(i)
+
 tcp.close()
