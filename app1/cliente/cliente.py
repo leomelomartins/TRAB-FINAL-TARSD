f8fb4fddb4b73dcf637e937d75eca4ec7ccce778
bruno <brunobaradel@gmail.com>
2019-07-08 23:01:36 -0300

    agora vai 100%

--- app1/cliente/cliente.py
+++ app1/cliente/cliente.py
@@ -7,7 +7,7 @@ tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp.connect(dest)
 
 fname = 'enviados/' + input() + '.txt'
-arq = open(fname, mode='r')
+arq = open(fname, mode='rb')
 
 for i in arq.readlines():
   tcp.send(i)
