61b7e0d10050f53023d0e0d8e84eb20c9e643ee1
bruno <brunobaradel@gmail.com>
2019-07-08 21:08:56 -0300

    aisjai

--- app1/cliente/cliente.py
+++ app1/cliente/cliente.py
@@ -6,7 +6,7 @@ dest = (HOST, PORT)
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp.connect(dest)
 
-msg = raw_input()
+msg = input()
 while msg != '\x18':
     tcp.send (msg)
     msg = raw_input()
