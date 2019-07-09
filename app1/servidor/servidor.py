fb702472bd5973e67f8653940d14b12b3c7dc7c9
bruno <brunobaradel@gmail.com>
2019-07-08 22:43:18 -0300

    agora vai 100%

--- app1/servidor/servidor.py
+++ app1/servidor/servidor.py
@@ -11,7 +11,7 @@ print ("Listening for client . . .")
 conn, address = tcp.accept()
 print ("Connected to client at ", address)
 
-arq = open('recebidos/foo.txt', 'w')
+arq = open('recebidos/recebido.txt', 'w')
 
 while True:
   output = conn.recv(2048)
