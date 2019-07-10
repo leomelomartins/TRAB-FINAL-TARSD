d1ce1621b259bd883dd17ad5b408e7a273e4b91d
bruno <brunobaradel@gmail.com>
2019-07-10 00:24:12 -0300

    aa

--- worker-config.sh
+++ worker-config.sh
@@ -2,14 +2,15 @@
 
 git clone https://github.com/leomelomartins/TRAB-FINAL-TARSD.git
 
+DEBIAN_FRONTEND=noninteractive sudo apt update
 
-sudo apt update
+#Instalacao do Docker
 sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt -y install docker-ce docker-ce-cli containerd.io
+DEBIAN_FRONTEND=noninteractive sudo apt update
+DEBIAN_FRONTEND=noninteractive sudo apt -y install docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
